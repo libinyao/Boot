36ddf47290855e3e869d92dc1390abbc86d75395
libingyao <577951474@qq.com>
2020-02-01 14:42:22 +0800

    init repo

---
+++ src/main/java/com/libingyao/y2020/LibingyaoApplication.java
@@ -0,0 +1,15 @@
+package com.libingyao.y2020;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibingyaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibingyaoApplication.class, args);
+    }
+
+}
+
+
---
+++ src/test/java/com/libingyao/y2020/LibingyaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.libingyao.y2020;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibingyaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
